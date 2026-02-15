ef66bff9a701f7cf8b3ac19b802d406283dcaf7a
ashiriag <40843474+ashiriag@users.noreply.github.com>
2026-02-14 22:05:38 -0800

    edit app for render

--- zoom/app.py
+++ zoom/app.py
@@ -52,4 +52,5 @@ def select_character():
     return render_template("index.html", selected_char=selected)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
